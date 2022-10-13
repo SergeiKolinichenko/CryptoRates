9afcb9e0d6ac8931d43b9c556d4a58a47220b2eb
Sergei Kolinichenko <iamserj61@gmail.com>
2022-10-13 18:40:59 +0300

    Start creating an app

---
+++ app/src/main/java/info/sergeikolinichenko/cryptorates/MainActivity.kt
@@ -0,0 +1,16 @@
+package info.sergeikolinichenko.cryptorates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/****************************************************
+* Real-time crypto rate app                         *
+* created by Sergey Kolinichenko 13.10.2022 16:16   *
+ ****************************************************/
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
